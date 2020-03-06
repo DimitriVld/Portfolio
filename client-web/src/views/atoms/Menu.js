0a8c6fe2b5e33ee4b0f5c0754c37e938696ff1f5
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-03-06 20:52:13 +0100

    Front | Init menu

---
+++ client-web/src/views/atoms/Menu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <div className={'menu-atom'}>
+            <p> This is a Menu atom </p>
+        </div>
+    )
+}
+
+export default Menu;
--- client-web/src/views/layouts/DefaultLayout.js
+++ client-web/src/views/layouts/DefaultLayout.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../atoms/Header';
+import Menu from '../atoms/Menu';
 
 const DefaultLayout = (props) => {
     return (
         <div className={"default-layout"}>
             <Header />
+            <Menu />
             <main className={"main-content"}>{props.page}</main>
         </div>
     );
