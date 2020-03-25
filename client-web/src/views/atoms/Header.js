4455ccc70f8ce2fb1f6de6c512117f44f4d6a4e0
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-03-25 21:05:04 +0100

    Front | Add scrollmagic

--- client-web/src/views/atoms/Header.js
+++ client-web/src/views/atoms/Header.js
@@ -3,11 +3,9 @@ import AppContext from "../../context/AppContext";
 import Icon from './icons/Index';
 
 class Header extends React.Component{
-    static contextType = AppContext;
 
     toggleMenu = () => {
-        this.context.showMenu = !this.context.showMenu;
-        console.log(this.context.showMenu)
+        document.querySelector('.menu-atom').classList.toggle('display-none-dft');
         document.querySelector('.burger-icon').classList.toggle('active');
     }
 
@@ -21,7 +19,7 @@ class Header extends React.Component{
                 <div className="header-buttons">
                     {/* Translate */}
                     <div className="translate">
-                        <p>text</p>
+                        <button>Fr</button> / <button>En</button>
                     </div>
 
                     {/* Burger menu */}
