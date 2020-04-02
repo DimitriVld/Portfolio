c9b96c8dd011d9acdd9af8808c4c35f525ad39ba
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-04-02 14:14:08 +0200

    Front | single work without styles

--- client-web/src/views/constants/staticProjects.js
+++ client-web/src/views/constants/staticProjects.js
@@ -7,7 +7,14 @@ const staticProjects = () => {
             title: 'First project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
         {
             id: 2,
@@ -15,35 +22,70 @@ const staticProjects = () => {
             title: '2 project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
         {
             id: 3,
             title: '3 project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
         {
             id: 4,
             title: '4 project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
         {
             id: 5,
             title: '5 project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
         {
             id: 6,
             title: '6 project',
             img: './images/defaultProject.png',
             type: 'Site vitrine',
+            link: 'https://www.google.com/',
             content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+            techno:[
+                {
+                    id: 1,
+                    name: 'ReactJs'
+                }
+            ]
         },
 
     ];
--- client-web/src/views/pages/SingleWork.js
+++ client-web/src/views/pages/SingleWork.js
@@ -13,8 +13,36 @@ class SingleWorkPage extends React.Component {
     render(){
         const work = this.state.works.find(element => element.id === this.state.id);
         return (
-            <div className={'single-work-page'}>
-                <p>{work.id}</p>
+            <div className={'single-work-page container-dft'}>
+
+                {/* div header */}
+                <div className='project-header'>
+                    <p className='type'>{work.type}</p>
+                    <h2 className='name'>{work.title}</h2>
+                </div>
+
+                {/* content */}
+                <div className='project-content'>
+
+                    {/* description */}
+                    <div className='description'>
+                        <p className='abstract'>{work.content}</p>
+                        <div className='technos'>
+                            <p>Techno we used</p>
+                            <ul>
+                                {work.techno.map((element, index) => {
+                                    return <li key={index}>{element.name}</li>
+                                })}
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Iframe */}
+                    <img className="img-work" src={work.img} alt="img work" />
+                </div>
+
+                {/* Link */}
+                <a href={work.link}>Go to the website</a>
             </div>
         )
     }
