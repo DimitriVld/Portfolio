9af07f491a37a7e57fab162d87e60487d95f912e
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-04-02 12:25:40 +0200

    Front | Add third section in servicesPage

---
+++ client-web/src/views/constants/staticSkills.js
@@ -0,0 +1,68 @@
+const staticSkills = () => {
+
+    return [
+
+        {
+            id: 1,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 2,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 3,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 4,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 5,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 6,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 7,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 8,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 9,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+
+        {
+            id: 10,
+            title: 'Name skill',
+            icon: './images/skills/php.svg',
+        },
+    ];
+
+};
+
+export default staticSkills;
--- client-web/src/views/molecules/ServicesPage/JobsSection.js
+++ client-web/src/views/molecules/ServicesPage/JobsSection.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import staticSkills from "../../constants/staticSkills";
 
-const JobsSection = () => {
-    return (
-        <div className={'jobs-section'}>
-            <p> This is a Jobs section </p>
-        </div>
-    )
+class JobsSection extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            skills: staticSkills(),
+        }
+    }
+
+    render(){
+        const skill = this.state.skills.map((skill, index) => {
+            return(
+                <img key={index} className="icon" src={skill.icon} alt="icon"/>
+            )
+        })
+        return (
+            <div className={'jobs-section'}>
+                <div className="bg"></div>
+                <div className='description'>
+                    <h3><hr></hr>Our techno</h3>
+                    <p>We gather a panel of techno to succeed at best all of our projects</p>
+                </div>
+
+                <div className='icons'>
+                    {skill}
+                </div>
+            </div>
+        )
+    }
 }
 
 export default JobsSection;
