74da0df73ad2d378ed477d0616240b4f3ff1c236
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-04-02 16:33:31 +0200

    Front | Add second section in About-us Page

---
+++ client-web/src/views/constants/staticCultures.js
@@ -0,0 +1,33 @@
+const staticCultures = () => {
+
+    return [
+
+        {
+            id: 1,
+            title: '#First culture',
+            content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+        },
+
+        {
+            id: 2,
+            title: '#First culture',
+            content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+        },
+
+        {
+            id: 3,
+            title: '#First culture',
+            content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+        },
+
+        {
+            id: 4,
+            title: '#First culture',
+            content: 'His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.',
+        },
+
+    ];
+
+};
+
+export default staticCultures;
--- client-web/src/views/molecules/AboutPage/CultureSection.js
+++ client-web/src/views/molecules/AboutPage/CultureSection.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import staticCultures from '../../constants/staticCultures';
 
-const CultureSection = () => {
-    return (
-        <div className={'culture-section'}>
-            <p> This is a Culture section </p>
-        </div>
-    )
+class CultureSection extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cultures: staticCultures(),
+        };
+    }
+
+    render(){
+        const culture = this.state.cultures.map((culture, index) => {
+            return(
+                <div key={index} className='culture'>
+                    <h4>{culture.title}</h4>
+                    <p>{culture.content}</p>
+                </div>
+            )
+        })
+
+        return (
+            <div className={'culture-section'}>
+                <div className='container-dft content'>
+                    <h3>Our culture,<br/> values and <br/>belief</h3>
+                    <div className='cultures'>
+                        {culture}
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default CultureSection;
