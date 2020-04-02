690db7ef2b49b69d225287bd3b50a1babe019d4f
Dimitri Vildina <dimitri.vildina@gmail.com>
2020-04-02 16:45:13 +0200

    First | Add last section in About us Page

--- client-web/src/views/molecules/AboutPage/ContactSection.js
+++ client-web/src/views/molecules/AboutPage/ContactSection.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const ContactSection = () => {
     return (
-        <div className={'contact-section'}>
-            <p> This is a Contact section </p>
+        <div className={'contact-section padding-t-b-dft'}>
+            <div className='container-dft content'>
+                <h3>Work with us</h3>
+                <h4>Let’s work together to build something great.</h4>
+                <Link to={'/contact'}>
+                    Contact us 
+                    <img className="icon-arrow border-rounded-dft" src="./images/icon_arrow.svg" alt="Arrow" />
+                </Link>
+            </div>
         </div>
     )
 }
