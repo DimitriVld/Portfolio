2d21d26eb0d175186f9bd91880ec6b4b2caaebc4
Dimitri Vildina <Dimitri.vildina@gmail.com>
2021-01-13 16:32:13 +0100

    Restart animations

--- client-web/src/views/atoms/Header.js
+++ client-web/src/views/atoms/Header.js
@@ -4,52 +4,55 @@ import { TimelineLite } from "gsap";
 import Icon from './icons/Index';
 
 class Header extends React.Component{
-    constructor(props) {
-        super(props);
-        this.myTween = new TimelineLite({ paused: true })
-        this.state = {
-            showMenu: false,
-        }
+  constructor(props) {
+    super(props);
+    this.myTween = new TimelineLite({ paused: true })
+    this.state = {
+      showMenu: false,
     }
+  }
 
-    animationMenu = () => {
-        const menuElement = document.querySelector(".menu-atom");
-        return(
-            this.myTween
-                .to(menuElement, 1, {bottom: 0})
-                .to(menuElement, 1, { width: '100%', ease: "expo"})
-        )
-    }
+  animationMenu = () => {
+    const menuElement = document.querySelector(".menu-atom");
+    const menuLinks = document.querySelectorAll(".link-menu");
+    const menuLinksArr = Array.from(menuLinks);
+    return(
+      this.myTween
+        .to(menuElement, .5, {bottom: 0})
+        .to(menuElement, .7, { width: '100%', ease: "expo"})
+        .staggerTo(menuLinksArr, .7, {opacity: 1, transform: 'translateX(0)'}, 0.2)
+    )
+  }
 
-    toggleMenu = () => {
-        this.state.showMenu ? this.animationMenu().reverse() : this.animationMenu().play();
-        this.setState({ showMenu: !this.state.showMenu });
-        document.querySelector('.burger-icon').classList.toggle('active');
-    }
+  toggleMenu = () => {
+    this.state.showMenu ? this.animationMenu().reverse() : this.animationMenu().play();
+    this.setState({ showMenu: !this.state.showMenu });
+    document.querySelector('.burger-icon').classList.toggle('active');
+  }
 
-    render() {
-        return (
-            <div className={"header-atom"} id='my-element'>
-                {/* Logo */}
-                <Link to={'/'}>
-                    <img className="header-logo" src="./images/logo.svg" alt="logo" />
-                </Link>
+  render() {
+    return (
+      <div className={"header-atom"} id='my-element'>
+        {/* Logo */}
+        <Link to={'/'}>
+          <img className="header-logo" src="./images/logo.svg" alt="logo" />
+        </Link>
 
-                {/* Header buttons */}
-                <div className="header-buttons">
-                    {/* Translate */}
-                    <div className="translate">
-                        <button>Fr</button> / <button>En</button>
-                    </div>
+        {/* Header buttons */}
+        <div className="header-buttons">
+          {/* Translate */}
+          <div className="translate">
+            <button>Fr</button> / <button>En</button>
+          </div>
 
-                    {/* Burger menu */}
-                    <div className="burger-menu border-rounded-dft" onClick={() => this.toggleMenu()}>
-                        <Icon name="Burger" width={30} stroke={"#fff"} />
-                    </div>
-                </div>
-            </div>
-        );
-    }
+          {/* Burger menu */}
+          <div className="burger-menu border-rounded-dft" onClick={() => this.toggleMenu()}>
+            <Icon name="Burger" width={30} stroke={"#fff"} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Header;
--- client-web/src/views/atoms/Menu.js
+++ client-web/src/views/atoms/Menu.js
@@ -13,11 +13,11 @@ class Menu extends React.Component {
         return (
             <div className={'menu-atom'}>
                 <nav className='nav'>
-                    <Link to='/'>Homepage</Link>
-                    <Link to='/works'>Our works</Link>
-                    <Link to='/services'>Our Services</Link>
-                    <Link to='/about-us'>About Us</Link>
-                    <Link to='/contact'>Contact us</Link>
+                    <Link to='/' className={'link-menu'}>Homepage</Link>
+                    <Link to='/works' className={'link-menu'}>Our works</Link>
+                    <Link to='/services' className={'link-menu'}>Our Services</Link>
+                    <Link to='/about-us' className={'link-menu'}>About Us</Link>
+                    <Link to='/contact' className={'link-menu'}>Contact us</Link>
                 </nav>
             </div>
         )
